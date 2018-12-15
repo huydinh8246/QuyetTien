ca254269b16a003b7a3d0f651c3e46cfa4ad4a94
manhhung <nguyenmanhhung9b@gmail.com>
2018-12-15 13:03:59 +0700

    Fix AdminLayout

---
+++ QuyetTien/QuyetTien/Controllers/CashBillController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuyetTien.Controllers
+{
+    public class CashBillController : Controller
+    {
+        // GET: CashBill
+        public ActionResult ViewListCB()
+        {
+            return View();
+        }
+    }
+}
