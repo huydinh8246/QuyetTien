0e3b94b5356da57840ea99608df851a8a8f55c0d
manhhung <nguyenmanhhung9b@gmail.com>
2018-12-15 13:35:31 +0700

    Add viewListCB

--- QuyetTien/QuyetTien/Controllers/CashBillController.cs
+++ QuyetTien/QuyetTien/Controllers/CashBillController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QuyetTien.Models;
 
 namespace QuyetTien.Controllers
 {
     public class CashBillController : Controller
     {
+        DmQT08Entities db = new DmQT08Entities();
         // GET: CashBill
         public ActionResult ViewListCB()
         {
-            return View();
+            var listCashBill = db.CashBills.OrderByDescending(n => n.ID);
+            return View(listCashBill);
         }
     }
 }
