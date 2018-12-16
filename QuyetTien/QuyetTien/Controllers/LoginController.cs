fcee1de47cb29682ca6575608590a2e41552958a
manhhung <nguyenmanhhung9b@gmail.com>
2018-12-16 15:09:41 +0700

    Add login layout

---
+++ QuyetTien/QuyetTien/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuyetTien.Models;
+
+namespace QuyetTien.Controllers
+{
+    public class LoginController : Controller
+    {
+        DmQT08Entities db = new DmQT08Entities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
